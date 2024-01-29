114247efaf4ed54d74f99f3761b40035ffcaf80f
dev-ismail0107 <146998319+dev-ismail0107@users.noreply.github.com>
2024-01-29 03:33:35 -0800

    Add files via upload

---
+++ final-project/src/components/Right/Right.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Right = () => {
+  return (
+    <div>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+        <h1>Right</h1>
+    </div>
+  )
+}
+
+export default Right
